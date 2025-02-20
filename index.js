29f3eac9e9537776e3e1ae93dedb83b3c128091b
Takmim00 <takmimm@gmail.com>
2025-02-20 12:09:31 +0600

    socjket io

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const app = express();
+const http = require('http');
+const {Server}= require("socket.io")
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const server = http.createServer(app)
+const io = new Server(server,{
+  cors:{
+    origin:"http://localhost:5000",
+    methods:["GET","POST"]
+  }
+})
 
 app.use(cors());
 app.use(express.json());
