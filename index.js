388165e300838d176f36227489118339a32e2515
Takmim00 <takmimm@gmail.com>
2025-02-21 18:59:01 +0600

    vercel

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST","DELETE"],
+    origin: ["*"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
+  transports: ["websocket"]
 });
 
 app.use(cors());
 app.use(express.json());
+
+module.exports = app;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@taskmanagement.lc85r.mongodb.net/?retryWrites=true&w=majority&appName=taskManagement`;
 
@@ -36,7 +40,7 @@ async function run() {
 
     const db = client.db("taskManagement");
     const userCollection = db.collection("users");
-    const messagesCollection = db.collection("messages"); // New Collection for Messages
+    const messagesCollection = db.collection("messages"); 
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -61,6 +65,10 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+    app.get("/tasks", async (req, res) => {
+      const result = await messagesCollection.find().toArray();
+      res.send(result);
+    });
 
     app.get("/messages/:category", async (req, res) => {
       const { category } = req.params;
@@ -72,10 +80,10 @@ async function run() {
     });
 
     io.on("connection", (socket) => {
-      console.log(`User Connected: ${socket.id}`);
+      // console.log(`User Connected: ${socket.id}`);
       socket.on("join_category", async (category) => {
         socket.join(category);
-        console.log(`User with ID: ${socket.id} joined category: ${category}`);
+        // console.log(`User with ID: ${socket.id} joined category: ${category}`);
         const messages = await messagesCollection
           .find({ category })
           .sort({ timestamp: 1 })
@@ -83,25 +91,24 @@ async function run() {
         socket.emit("previous_messages", messages);
       });
 
-      socket.on("send_message", async (data) => {
+      socket.on("send_task", async (data) => {
         const { category, title } = data;
-      
+
         const newMessage = {
           category,
           title,
           timestamp: new Date(),
         };
-      
+
         const result = await messagesCollection.insertOne(newMessage);
-      
+
         const insertedTask = {
           _id: result.insertedId,
           ...newMessage,
         };
-      console.log(insertedTask);
-        socket.to(category).emit("receive_message", insertedTask);
+        // console.log(insertedTask);
+        socket.to(category).emit("receive_task", insertedTask);
       });
-      
 
       socket.on("delete_task", async (data) => {
         const { id, category } = data;
@@ -134,10 +141,10 @@ async function run() {
           title: req.body.title,
           timestamp: new Date(),
         };
-    
+
         const result = await messagesCollection.insertOne(task);
-        task._id = result.insertedId; 
-    
+        task._id = result.insertedId;
+
         res.json(task);
       } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
@@ -147,48 +154,50 @@ async function run() {
       try {
         const id = req.params.id;
         console.log("Deleting task with ID:", id);
-    
+
         if (!id || !ObjectId.isValid(id)) {
           return res.status(400).json({ message: "Invalid ObjectId" });
         }
-    
-        const result = await messagesCollection.deleteOne({ _id: new ObjectId(id) });
-    
+
+        const result = await messagesCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+
         if (result.deletedCount === 0) {
           return res.status(404).json({ message: "Task not found" });
         }
-    
+
         res.json({ success: true, message: "Task deleted successfully" });
       } catch (error) {
         console.error("Error deleting task:", error);
         res.status(500).json({ message: "Error deleting task", error });
       }
     });
-    
+
     app.put("/tasks/:id", async (req, res) => {
-      const  id  =  req.params.id;
+      const id = req.params.id;
       const updatedMessage = req.body;
-    
+
       try {
         const filter = { _id: new ObjectId(id) };
         const updateDoc = { $set: updatedMessage };
-    
+
         const result = await messagesCollection.updateOne(filter, updateDoc);
-    
+
         if (result.modifiedCount > 0) {
           const updatedTask = await messagesCollection.findOne(filter);
-          io.emit("edit_task", updatedTask); 
+          io.emit("edit_task", updatedTask);
           res.send(updatedTask);
         } else {
-          res.status(404).send({ message: "Message not found or no changes made." });
+          res
+            .status(404)
+            .send({ message: "Message not found or no changes made." });
         }
       } catch (error) {
         console.error("Error updating message:", error);
         res.status(500).send({ message: "Internal Server Error" });
       }
     });
-    
-    
 
     console.log("Connected to MongoDB!");
   } finally {
